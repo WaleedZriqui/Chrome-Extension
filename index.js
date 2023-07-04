d1e37acaa379a6c8bcb756f5c4a200de5beda8bd
WaleedZriqui <waleedzriqui@gmail.com>
2023-07-05 02:41:51 +0300

    Adding mainfest file

--- index.js
+++ index.js
@@ -14,9 +14,11 @@ function renderLeads() {
 
     for (let i = 0; i < myLeads.length; i++) {
         let temStr = `            
-        <li>
-        <a target='_blank' href='${myLeads[i]}'>${myLeads[i]}</a>
-        </li>
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
         ` // this is Template Literals(``) that allow to have multuple lines in js with using ${} to get the value 
         listItems += temStr
 
