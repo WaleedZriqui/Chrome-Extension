398c5184cda63cf57dae7f2dc045b2ab769c85a1
WaleedZriqui <waleedzriqui@gmail.com>
2023-07-06 02:40:55 +0300

    Add save tab button

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let myLeads = []
 const inputEl = document.querySelector("#input-el")
 const inputBtn = document.querySelector("#input-btn")
 const ulEl = document.querySelector("#ul-el")
+const tabBtn = document.querySelector("#tab-btn")
 const deleteBtn = document.querySelector("#delete-btn")
 
 const leadsFromLocalStorger = localStorage.getItem("myLeads")
@@ -16,6 +17,14 @@ inputBtn.addEventListener("click", function () {
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
     render(myLeads)
     inputEl.value = ""
+})
+
+tabBtn.addEventListener("click", function () {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
 })
 
 deleteBtn.addEventListener("dblclick", function () {
