ad55160a9496ba68b24b462777288b50f3b707eb
WaleedZriqui <waleedzriqui@gmail.com>
2023-07-05 21:44:09 +0300

    Read and Write from local storage

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ const inputEl = document.querySelector("#input-el")
 const inputBtn = document.querySelector("#input-btn")
 const ulEl = document.querySelector("#ul-el")
 
+let leadsFromLocalStorger = localStorage.getItem("myLeads")
+
+if (leadsFromLocalStorger) {
+    myLeads = leadsFromLocalStorger
+    renderLeads()
+}
+
 inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
     renderLeads()
     inputEl.value = ""
 })
